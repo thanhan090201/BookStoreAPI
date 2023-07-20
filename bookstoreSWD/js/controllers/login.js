5967d75ae43c63e5578e4407d5b30bafb8da3652
thanhan090201 <andvtse151492@fpt.edu.vn>
2023-07-20 18:20:59 +0700

    new login

--- bookstoreSWD/js/controllers/login.js
+++ bookstoreSWD/js/controllers/login.js
@@ -1,52 +1,46 @@
 async function login() {
-  const url = "https://book0209.azurewebsites.net/api/user/getUser";
-
-  const getTodo = async (url) => {
-    return await fetch(url);
-  };
+  const url = "https://book0209.azurewebsites.net/api/user/login"; // Replace with your actual API endpoint
 
   var username = document.getElementById("username").value;
+  console.log("username: ", username);
   var password = document.getElementById("password").value;
+  console.log("password: ", password);
 
-  const todo = await getTodo(url);
-  const data = await todo.json();
+  // Data to be sent in the request body
+  const data = {
+    user_Account: username,
+    user_Password: password,
+  };
 
-  console.log(data);
+  // Configuration for the fetch request
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      // Add any other required headers here
+    },
+    body: JSON.stringify(data),
+  };
 
-  data.map((item) => {
-    if (
-      item.user_Account == username &&
-      item.user_Password == password &&
-      item.is_User_Status == true &&
-      item.role_Id == 1
-    ) {
-      localStorage.setItem("user", JSON.stringify(item));
-      window.location.href = "/html/bookRequestAdmin.html";
-    } else if (
-      item.user_Account == username &&
-      item.user_Password == password &&
-      item.is_User_Status == true &&
-      item.role_Id == 2
-    ) {
-      localStorage.setItem("user", JSON.stringify(item));
-      window.location.href = "productlist.html";
-    } else if (
-      item.user_Account == username &&
-      item.user_Password == password &&
-      item.is_User_Status == true &&
-      item.role_Id == 3
-    ) {
-      localStorage.setItem("user", JSON.stringify(item));
-      window.location.href = "index.html";
-    } else if (item.user_Account.length == 0 && item.user_Password == 0) {
-      document.getElementById("notification").innerHTML =
-        "Please fill in username or password";
-    } else if (
-      item.user_Account != username &&
-      item.user_Password != password
-    ) {
-      document.getElementById("notification").innerHTML =
-        "Wrong username or password";
-    }
-  });
+  fetch(url, requestOptions)
+    .then((response) => {
+      if (response.status != 200) {
+        document.getElementById("notification").innerHTML =
+          "Incorect username or password";
+      }
+      return response.json();
+    }) // Parse response as JSON
+    .then((data) => {
+      localStorage.setItem("user", JSON.stringify(data));
+      if (data.role_Id == 1) {
+        window.location.href = "bookRequestAdmin.html";
+      } else if (data.role_Id == 2) {
+        window.location.href = "bookRequestStaff.html";
+      } else {
+        window.location.href = "index.html";
+      }
+    })
+    .catch((error) => {
+      console.error("Error occurred during login:", error);
+    });
 }
